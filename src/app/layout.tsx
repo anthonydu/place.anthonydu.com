9005afab29f213812279756c9d44aa3953925cd2
Anthony Du <github@anthonydu.com>
2023-07-19 13:36:57 +0800

    Added analytics tools

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import "./globals.css";
 import type { Metadata } from "next";
 
@@ -13,7 +14,48 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+
+        {/* Default Statcounter code for r/place https://place.anthonydu.com */}
+        <Script id="statcounter">
+          {`
+          var sc_project=12901741; 
+          var sc_invisible=1; 
+          var sc_security="4f65a7c4";
+        `}
+        </Script>
+        <Script
+          src="https://www.statcounter.com/counter/counter.js"
+          async
+        ></Script>
+        {/* End of Statcounter Code */}
+
+        {/* Hotjar Tracking Code for r/place */}
+        <Script id="hotjar">
+          {`
+          (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:3579231,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        `}
+        </Script>
+
+        <Script
+          async
+          data-id="101416538"
+          src="//static.getclicky.com/js"
+        ></Script>
+        <Script
+          data-goatcounter="https://anthonydu.goatcounter.com/count"
+          async
+          src="//gc.zgo.at/count.js"
+        ></Script>
+      </body>
     </html>
   );
 }
